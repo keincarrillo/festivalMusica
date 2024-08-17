7a753c2837131e08c362e92b79a8547617bcf69d
Kein <kein.carrillo.escom@gmail.com>
2024-08-16 22:26:22 -0600

    styles: for gallery

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-alert('ola')
+class Gallery {
+    generateGallery() {
+        const numberImages = 16;
+        const gallery = document.querySelector('#gallery');
+        for(let i = 1; i <= numberImages ; i++) {
+            const image = document.createElement('img');
+            image.src = `src/img/gallery/full/${i}.jpg`;
+            image.alt = 'Imagen Gallery';
+            gallery.append(image);
+        }
+    }
+}
+
+const ui = new Gallery();
+console.log(ui.generateGallery());
